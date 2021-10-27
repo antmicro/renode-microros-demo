e70ec93d5ff24e4df3c25fa327c0ea8a204d0832
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-10-27 10:22:18 +0200

    [#29751] main: Added node declaration
    Added a declaration of a basic MicroROS node
    
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- src/microros-sensor-publisher-subscriber/src/main.c
+++ src/microros-sensor-publisher-subscriber/src/main.c
@@ -9,5 +9,17 @@ int main()
   rclc_support_t support;
   rcl_ret_t return_code = rclc_support_init(&support, argc, argv, &def_allocator);
 
+  rcl_node_t node;
+  const char* node_name = "microros_sensor_publisher";
+  const char* namespace = "default_namespace";
+
+  return_code = rclc_node_init_default(&node, node_name, namespace, &support);
+  if(return_code != RCL_RET_OK)
+  {
+    // TODO: Add some sort of error handling
+    return -1;
+  }
+  rcl_node_fini(&node);
+
   return 0;
 }
