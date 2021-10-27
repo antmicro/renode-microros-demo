fe88c2e69f73e1fdfe59cab8713c523a31937705
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-10-27 10:23:00 +0200

    [#29751] main: Added publisher declaration
    Added a basic publisher declaration
    
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- src/microros-sensor-publisher-subscriber/src/main.c
+++ src/microros-sensor-publisher-subscriber/src/main.c
@@ -5,6 +5,8 @@
 
 int main()
 {
+  // add all initializations: allocator, support and node
+  
   rcl_allocator_t def_allocator = rcl_get_default_allocator();
   rclc_support_t support;
   rcl_ret_t return_code = rclc_support_init(&support, argc, argv, &def_allocator);
@@ -13,12 +15,33 @@ int main()
   const char* node_name = "microros_sensor_publisher";
   const char* namespace = "default_namespace";
 
+  rcl_publisher_t publisher;
+  const char* topic_name = "microros_sensor_topic";
+  const rosdil_message_type_support_t* type_support = 
+    ROSDIL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32);
+  
+
   return_code = rclc_node_init_default(&node, node_name, namespace, &support);
   if(return_code != RCL_RET_OK)
   {
     // TODO: Add some sort of error handling
     return -1;
   }
+
+  return_code = rclc_publisher_init_default(
+      &publisher,
+      &node,
+      &type_suppeort,
+      &topic_name
+  );
+  
+  if(return_code != RCL_RET_OK)
+  {
+    return -2;
+  }
+  // cleanup
+  
+  rcl_publisher_fini(&publisher, &node);
   rcl_node_fini(&node);
 
   return 0;
