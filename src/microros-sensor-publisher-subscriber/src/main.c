085e97f4108e439dc254e24d704df93b59ee026f
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-11-03 13:50:33 +0100

    [#29751] main.c: Fixed includes and mistakes
    Fixed include mistakes, adding or deleting headers as required. Also,
    added a 'cleaning up' printf to easily indentify if the execution
    reached the end
    
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- src/microros-sensor-publisher-subscriber/src/main.c
+++ src/microros-sensor-publisher-subscriber/src/main.c
@@ -1,6 +1,14 @@
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/header.h>
+#include <std_msgs/msg/float64.h>
+
 #include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
 #include <zephyr.h>
 
 int main()
@@ -9,7 +17,7 @@ int main()
   
   rcl_allocator_t def_allocator = rcl_get_default_allocator();
   rclc_support_t support;
-  rcl_ret_t return_code = rclc_support_init(&support, argc, argv, &def_allocator);
+  rcl_ret_t return_code = rclc_support_init(&support, 0, NULL, &def_allocator);
 
   rcl_node_t node;
   const char* node_name = "microros_sensor_publisher";
@@ -17,9 +25,6 @@ int main()
 
   rcl_publisher_t publisher;
   const char* topic_name = "microros_sensor_topic";
-  const rosdil_message_type_support_t* type_support = 
-    ROSDIL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32);
-  
 
   return_code = rclc_node_init_default(&node, node_name, namespace, &support);
   if(return_code != RCL_RET_OK)
@@ -31,8 +36,8 @@ int main()
   return_code = rclc_publisher_init_default(
       &publisher,
       &node,
-      &type_suppeort,
-      &topic_name
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64),
+      topic_name
   );
   
   if(return_code != RCL_RET_OK)
@@ -41,17 +46,17 @@ int main()
   }
 
   //publish a message
-  std_msgs__msg__Float32 msg;
+  std_msgs__msg__Float64 msg;
   msg.data = 11.22;
-  return_code = rcl_publish(&publisher, &message, NULL);
+  return_code = rcl_publish(&publisher, &msg, NULL);
   while(return_code != RCL_RET_OK)
   {
     // try to publish untill success
-    return_code = rcl_publish(&publisher, &message, NULL);
+    return_code = rcl_publish(&publisher, &msg, NULL);
   }
 
   // cleanup
-  
+  printf("Cleaning up\n");
   rcl_publisher_fini(&publisher, &node);
   rcl_node_fini(&node);
 
