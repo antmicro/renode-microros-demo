9b417e4ad5281181ee94d2439c34cc3be97cc3e0
Jan Bylicki <jbylicki@internships.antmicro.com>
2021-12-13 18:08:06 +0100

    [#30208] subscriber_node: Changed indentation type
    Signed-off-by: Jan Bylicki <jbylicki@internships.antmicro.com>

--- src/ros2-sensor-subscriber/src/subscriber_node.cpp
+++ src/ros2-sensor-subscriber/src/subscriber_node.cpp
@@ -6,27 +6,34 @@ using std::placeholders::_1;
 
 class Subscriber : public rclcpp::Node
 {
-  public:
-    Subscriber() : Node("Subscriber")
-    {
-      subscription_ = this->create_subscription<std_msgs::msg::Float64>(
-      "default_namespace/microros_sensor_topic", 10, std::bind(&Subscriber::topic_callback, this, _1));
-    }
-  private:
-    void topic_callback(const std_msgs::msg::Float64::SharedPtr msg) const
-    {
-      printf(
-        "Got from MicroROS: '%lf'\n",
-        msg->data
-      );
-    }
-    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_;
+    public:
+        Subscriber() : Node("Subscriber")
+        {
+            subscription_ = this->create_subscription<std_msgs::msg::Float64>(
+                "default_namespace/microros_sensor_topic",
+                10,
+                std::bind(
+                    &Subscriber::topic_callback,
+                    this,
+                    _1
+                )
+            );
+        }
+    private:
+        void topic_callback(const std_msgs::msg::Float64::SharedPtr msg) const
+        {
+            printf(
+                "Got from MicroROS: '%lf'\n",
+                msg->data
+            );
+        }
+        rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Subscriber>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Subscriber>());
+    rclcpp::shutdown();
+    return 0;
 }
